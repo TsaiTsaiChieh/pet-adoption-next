1127274bdb61723d5a3c6d286ef7cd1f067c5adb
Tsai-Chieh <d0177381@gmail.com>
2024-10-13 21:23:27 +0800

    :construction: add color and init the nav

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,5 +1,15 @@
+import { MdPets } from "react-icons/md";
+import styles from "./Navbar.module.css";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.nav__iconContent}>
+        <MdPets size={28} />
+        <h1 className={styles.nav__title}>認養毛孩</h1>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
