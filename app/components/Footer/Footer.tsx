df094de342e25b23fa3fdf57f5e800b3bd93f816
Tsai-Chieh <d0177381@gmail.com>
2024-10-24 21:41:55 +0800

    :sparkles: update Footer style

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,27 +1,35 @@
+import { Flex } from "@chakra-ui/react";
 import styles from "./Footer.module.scss";
 import { FaGithubAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footer__contactWrap}>
-        <FaGithubAlt size={28} />
-        <div className={styles.footer__contact}>
-          <a
-            href='https://github.com/TsaiTsaiChieh/pet-adoption-next/issues'
-            target='_blank'
-            className={styles.footer__report}
-          >
-            問題回報
-          </a>
-          <a
-            href='mailto:jecica196@gmail.com'
-            className={styles.footer__contactMe}
-          >
-            聯絡我
-          </a>
-        </div>
-      </div>
+      <Flex className={styles.footer__content}>
+        <Flex className={styles.footer__contactWrap}>
+          <FaGithubAlt size={28} />
+          <div className={styles.footer__contact}>
+            <a
+              href='https://github.com/TsaiTsaiChieh/pet-adoption-next/issues'
+              target='_blank'
+              className={styles.footer__report}
+            >
+              問題回報
+            </a>
+            <a
+              href='mailto:jecica196@gmail.com'
+              className={styles.footer__contactMe}
+            >
+              聯絡我
+            </a>
+          </div>
+        </Flex>
+        <Flex className={styles.footer__warningWrap}>
+          <span className={styles.footer__warning}>
+            本網站僅提供寵物資訊的檢索服務，不對其正確性或即時性負責，亦對認養等相關爭議或損失概不負責。
+          </span>
+        </Flex>
+      </Flex>
     </footer>
   );
 };
