98412a0b7276764f021a441eef5ee0f8b4425cbf
Tsai-Chieh <d0177381@gmail.com>
2024-11-03 15:30:53 +0800

    :sparkles: add prefix

--- app/components/pet/LabelBox/LabelBox.tsx
+++ app/components/pet/LabelBox/LabelBox.tsx
@@ -4,12 +4,22 @@ import { Flex, Text } from "@chakra-ui/react";
 interface Props {
   label: string;
   value: string;
+  prefix?: string;
 }
-const LabelBox = ({ label, value }: Props) => {
+const LabelBox = ({ label, value, prefix }: Props) => {
   return (
     <Flex className={styles.labelBox}>
       <Text className={styles.labelBox__heading}>{label}</Text>
-      <Text className={styles.labelBox__value}>{value.substring(0, 3)}</Text>
+      <Text
+        className={`${styles.labelBox__value} ${
+          prefix ? styles["labelBox__value--prefix"] : ""
+        }`}
+      >
+        {value.substring(0, 3)}
+        {prefix && (
+          <span className={styles.labelBox__prefix}>{prefix}</span>
+        )}{" "}
+      </Text>
     </Flex>
   );
 };
