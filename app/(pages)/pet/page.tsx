1eea31717b3a1c06f3687be16c22d66b9579b6ad
Tsai-Chieh <d0177381@gmail.com>
2024-11-03 17:27:13 +0800

    :recycle: refactor

--- app/(pages)/pet/page.tsx
+++ app/(pages)/pet/page.tsx
@@ -19,29 +19,28 @@ const Pet = () => {
       <ScrollButton />
       {data?.length === 0 && <NotFound />}
       <Grid className={styles.petSection}>
-        {data &&
-          data.map(pet => (
-            <PetCard
-              isLoading={isLoading || isFetching}
-              key={pet.animal_id}
-              img={pet.album_file}
-              id={pet.animal_id}
-              subId={pet.animal_subid}
-              kind={pet.animal_kind}
-              color={pet.animal_colour}
-              place={pet.shelter_name}
-              sex={pet.animal_sex}
-              age={pet.animal_age}
-              phone={pet.shelter_tel}
-              address={pet.shelter_address}
-              remark={pet.animal_remark}
-              bodyType={pet.animal_bodytype}
-              bacterin={pet.animal_bacterin}
-              sterilization={pet.animal_sterilization}
-              updateDate={pet.animal_update}
-              createDate={pet.animal_createtime}
-            />
-          ))}
+        {data?.map(pet => (
+          <PetCard
+            isLoading={isLoading || isFetching}
+            key={pet.animal_id}
+            img={pet.album_file}
+            id={pet.animal_id}
+            subId={pet.animal_subid}
+            kind={pet.animal_kind}
+            color={pet.animal_colour}
+            place={pet.shelter_name}
+            sex={pet.animal_sex}
+            age={pet.animal_age}
+            phone={pet.shelter_tel}
+            address={pet.shelter_address}
+            remark={pet.animal_remark}
+            bodyType={pet.animal_bodytype}
+            bacterin={pet.animal_bacterin}
+            sterilization={pet.animal_sterilization}
+            updateDate={pet.animal_update}
+            createDate={pet.animal_createtime}
+          />
+        ))}
       </Grid>
       <Pagination />
     </>
