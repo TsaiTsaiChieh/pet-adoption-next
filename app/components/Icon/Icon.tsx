41e054450d6dbe22507074ef9098f9116146aa43
Tsai-Chieh <d0177381@gmail.com>
2024-11-03 16:57:11 +0800

    :sparkles: add Icon and filter css

---
+++ app/components/Icon/Icon.tsx
@@ -0,0 +1,22 @@
+import styles from "./Icon.module.scss";
+import Image from "next/image";
+import React from "react";
+
+interface Props {
+  src: string;
+  alt?: string;
+  size: number;
+}
+const Icon = ({ src, alt, size }: Props) => {
+  return (
+    <Image
+      className={styles.icon}
+      alt={alt ?? "icon"}
+      width={size}
+      height={size}
+      src={src}
+    ></Image>
+  );
+};
+
+export default Icon;
