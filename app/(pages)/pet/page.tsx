e4c3a10d50900c5ac78b0266cc8b077603a7267d
Tsai-Chieh <d0177381@gmail.com>
2024-11-09 18:52:01 +0800

    :sparkles: add Banner component

--- app/(pages)/pet/page.tsx
+++ app/(pages)/pet/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import styles from "./page.module.scss";
 import PetCard from "@app/components/pet/PetCard/PetCard";
-import { Grid } from "@chakra-ui/react";
+import { Flex, Grid } from "@chakra-ui/react";
 import { useGetPetByFilterQuery } from "@app/services/pet";
 import { useAppSelector } from "@lib/hooks";
 import TopFilter from "@app/components/pet/TopFilter/TopFilter";
 import NotFound from "@app/components/pet/NotFound/NotFound";
 import Pagination from "@app/components/pet/Pagination/Pagination";
 import ScrollButton from "@app/components/ScrollButton/ScrollButton";
+import Banner from "@app/components/Banner/Banner";
 
 const Pet = () => {
   const { filter } = useAppSelector(state => state.pet);
@@ -15,34 +16,37 @@ const Pet = () => {
 
   return (
     <>
-      <TopFilter />
       <ScrollButton />
-      {data?.length === 0 && <NotFound />}
-      <Grid className={styles.petSection}>
-        {data?.map(pet => (
-          <PetCard
-            isLoading={isLoading || isFetching}
-            key={pet.animal_id}
-            img={pet.album_file}
-            id={pet.animal_id}
-            subId={pet.animal_subid}
-            kind={pet.animal_kind}
-            color={pet.animal_colour}
-            place={pet.shelter_name}
-            sex={pet.animal_sex}
-            age={pet.animal_age}
-            phone={pet.shelter_tel}
-            address={pet.shelter_address}
-            remark={pet.animal_remark}
-            bodyType={pet.animal_bodytype}
-            bacterin={pet.animal_bacterin}
-            sterilization={pet.animal_sterilization}
-            updateDate={pet.animal_update}
-            createDate={pet.animal_createtime}
-          />
-        ))}
-      </Grid>
-      <Pagination />
+      <TopFilter />
+      <Flex className={styles.container}>
+        <Banner />
+        {data?.length === 0 && <NotFound />}
+        <Grid className={styles.petSection}>
+          {data?.map(pet => (
+            <PetCard
+              isLoading={isLoading || isFetching}
+              key={pet.animal_id}
+              img={pet.album_file}
+              id={pet.animal_id}
+              subId={pet.animal_subid}
+              kind={pet.animal_kind}
+              color={pet.animal_colour}
+              place={pet.shelter_name}
+              sex={pet.animal_sex}
+              age={pet.animal_age}
+              phone={pet.shelter_tel}
+              address={pet.shelter_address}
+              remark={pet.animal_remark}
+              bodyType={pet.animal_bodytype}
+              bacterin={pet.animal_bacterin}
+              sterilization={pet.animal_sterilization}
+              updateDate={pet.animal_update}
+              createDate={pet.animal_createtime}
+            />
+          ))}
+        </Grid>
+        <Pagination />
+      </Flex>
     </>
   );
 };
---
+++ app/components/Banner/Banner.tsx
@@ -0,0 +1,19 @@
+import styles from "./Banner.module.scss";
+
+const Banner = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__img} />
+      <a
+        target='_blank'
+        rel='noreferrer noopener'
+        href='https://www.buymeacoffee.com/never.stray'
+        className={styles.container__link}
+      >
+        贊助一杯咖啡，讓我們可以繼續提供服務
+      </a>
+    </div>
+  );
+};
+
+export default Banner;
