c9684a7db3e807084453aeb24dd30710e442be13
Tsai-Chieh <d0177381@gmail.com>
2024-11-16 17:21:27 +0800

    :fire: rm move2Top logic in ScrollButton component

--- app/components/ScrollButton/ScrollButton.tsx
+++ app/components/ScrollButton/ScrollButton.tsx
@@ -1,23 +1,8 @@
 import styles from "./ScrollButton.module.scss";
 import { IconButton } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
 import Icon from "@app/components/Icon/Icon";
 
 const ScrollButton = () => {
-  const [isAtTop, setIsAtTop] = useState(true);
-  const handleScroll = () => {
-    setIsAtTop(window.scrollY === 0);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-  const scroll2Top = () => {
-    window.scroll({ top: 0, behavior: "smooth" });
-  };
   const scroll2Bottom = () => {
     window.scroll({
       top: document.documentElement.scrollHeight,
@@ -29,13 +14,9 @@ const ScrollButton = () => {
     <IconButton
       className={styles.button}
       aria-label='scroll-top'
-      onClick={isAtTop ? scroll2Bottom : scroll2Top}
+      onClick={scroll2Bottom}
     >
-      {isAtTop ? (
-        <Icon src='/icons/circle-down.svg' size={32} />
-      ) : (
-        <Icon src='/icons/circle-up.svg' size={32} />
-      )}
+      <Icon src='/icons/circle-down.svg' size={32} />
     </IconButton>
   );
 };
